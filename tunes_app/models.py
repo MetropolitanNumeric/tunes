8e66792078b116d349fd243979013014fec93c5f
billbunkum <william.richard.hurst@gmail.com>
2016-09-22 15:24:52 -0400

    start models.py

--- tunes_app/models.py
+++ tunes_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=50)
+    artist = models.ForeignKey(Artist)
+    genre = models.ForeignKey(Genre)
+
+    def get_absolute_url(self):
+        #return reverse('namespace:name', args=[self.pk])
+        pass
+
+class Track(models.Model):
+    name = models.CharField(max_length=50)
+    album = models.ForeignKey(Album)
+    
+    def get_absolute_url(self):
+        #return reverse('namespace:name', args=[self.pk])
+        pass
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    album = models.ForeignKey(Album)

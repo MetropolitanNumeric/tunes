550a85735c6a6fd1166f593c2be1b82fad624c49
billbunkum <william.richard.hurst@gmail.com>
2016-09-22 18:32:04 -0400

    omg, i figured it out

--- tunes_app/views.py
+++ tunes_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.db.models import Count
 
 from .models import Album, Track, Artist, Genre
 
 def album_list(request):
-    albums = Album.objects.all()
+    albums = Album.objects.annotate(album_count=Count('artist')).all()
+# WHY DO WE HAVE TO USE 'ARTIST' WHEN def bookcase_list DOESN'T???????????
 
     context = {
         "albums": albums,
     }
     return render(request, "tunes_app/album_list.html", context)
 
-def album_detail(request):
-    pass
+def album_detail(request, id):
+    album = get_object_or_404(Album, pk=id)

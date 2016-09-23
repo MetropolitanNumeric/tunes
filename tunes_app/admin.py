effbe47f57481ae4dd337b763290a1c786ce24b3
billbunkum <william.richard.hurst@gmail.com>
2016-09-23 16:51:00 -0400

    make albums, tracks, artists; add admin.register(models)

--- tunes_app/admin.py
+++ tunes_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Album
+from .models import Album, Genre, Track, Artist
 
 admin.site.register(Album)
+admin.site.register(Genre)
+admin.site.register(Artist)

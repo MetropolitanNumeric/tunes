078a93e935e8a38fa8d0d9c0a6739960d5d4689d
billbunkum <william.richard.hurst@gmail.com>
2016-09-25 19:16:49 -0400

    holy hell album/2 works...bug in -> only loads album/2

--- tunes_app/views.py
+++ tunes_app/views.py
@@ -16,18 +16,12 @@ def tracks_in_album(request, id):
     specific_album = get_object_or_404(Album, pk=id)
 
 #   Tracks in a specific album
-    album_tracks = albums.track_set
+    album_tracks = Album.track_set
+    tracks = Track.objects.all()
 
     context = {
         "specific_album": specific_album,
         "album_tracks": album_tracks,
+        "tracks": tracks,
     }
     return render(request, "tunes_app/album_tracks.html", context)
-
-def get_all_tracks(request):
-    tracks = Track.objects.all()
-
-    context = {
-            "tracks": tracks,
-    }
-    return render(request, "tunes_app/complete_track_list.html", context)
